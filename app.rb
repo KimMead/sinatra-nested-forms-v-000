c37d0b30e5378a82321856a624494b939f01a48f
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-12-21 01:20:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,12 @@ module FormsLab
     post '/pirates' do
       params["pirate"]["ship"].each do |ship_data|
         Ship.new(ship_data["name"], ship_data["type"], ship_data["booty"])
-      end 
+      end
+
+      @pirate = Pirate.new(params["pirate"]["name"], params["pirate"]["height"], params["pirate"]["weight"])
+      @ship = Ship.all 
+      erb :'pirate/show'
+    end 
 
     # code other routes/actions here
 
